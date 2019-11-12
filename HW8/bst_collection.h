f9c7744062199af4d308929d0fff3ce9df967a5b
Gustiin <ericgustin44@gmail.com>
2019-11-11 18:29:43 -0800

    added HW8

---
+++ HW8/bst_collection.h
@@ -0,0 +1,325 @@
+/*Author: Eric Gustin
+  Assignment: CPSC223-01 HW08 bst_collection.h
+  Description: Binary Search Tree implementation of the
+  collection class. Contains all collection methods except for "remove".
+*/
+#ifndef BST_COLLECTION_H
+#define BST_COLLECTION_H
+
+#include <vector>
+#include "collection.h"
+
+
+template <typename K, typename V>
+class BSTCollection : public Collection<K,V>
+{
+public:
+
+ // create an empty linked list
+ BSTCollection();
+
+ // copy a linked list
+ BSTCollection(const BSTCollection<K,V>& rhs);
+
+ // assign a linked list
+ BSTCollection<K,V>& operator=(const BSTCollection<K,V>& rhs);
+
+ // delete a linked list
+ ~BSTCollection();
+
+ // insert a key - value pair into the collection
+ void insert(const K& key, const V& val);
+
+ // remove a key - value pair from the collection
+ void remove(const K& key);
+
+ // find the value associated with the key
+ bool find(const K& key, V& val) const;
+
+ // find the keys associated with the range
+ void find(const K& k1, const K& k2, std::vector<K>& keys) const;
+
+ // return all keys in the collection
+ void keys(std::vector<K>& keys) const;
+
+ // return collection keys in sorted order
+ void sort(std::vector<K>& keys) const;
+
+ // return the number of keys in collection
+ int size() const;
+
+ // return the height of the tree
+ int height() const;
+ 
+private:
+
+ // binary search tree node structure
+ struct Node {
+  K key;
+  V value;
+  Node* left;
+  Node* right;
+ };
+
+ // root node of the search tree
+ Node* root;
+
+ // number of k-v pairs in the collection
+ int collection_size;
+
+ // helper to recursively empty search tree
+ void make_empty(Node* subtree_root);
+
+ // helper to recursively build sorted list of keys
+ void inorder(const Node* subtree, std::vector<K>& keys) const;
+
+ // helper to recursively build sorted list of keys
+ void preorder(const Node* subtree, std::vector<K>& keys) const;
+
+ // helper to recursively find range of keys
+ void range_search(const Node* subtree, const K& k1, const K& k2,
+ std::vector<K>& keys) const;
+
+ // return the height of the tree rooted at subtree_root
+ int height(const Node* subtree_root) const;
+
+};
+
+
+template <typename K, typename V>
+BSTCollection<K,V>::BSTCollection() :
+collection_size(0), root(nullptr)
+{
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::make_empty(Node* subtree_root)
+{
+ // this is a recursive helper function
+ // base case
+ if (subtree_root == nullptr)
+  return;
+ // postorder delete
+ make_empty(subtree_root->left);
+ make_empty(subtree_root->right);
+  delete subtree_root;
+  subtree_root = nullptr;
+  --collection_size;
+}
+
+
+template <typename K, typename V>
+BSTCollection<K,V>::~BSTCollection()
+{
+ make_empty(root);
+}
+
+
+template <typename K, typename V>
+BSTCollection<K,V>::BSTCollection(const BSTCollection<K,V>& rhs)
+: collection_size(0), root(nullptr)
+{
+ *this = rhs;
+}
+
+
+template <typename K, typename V>
+BSTCollection <K,V>& BSTCollection<K,V>::operator=(const BSTCollection<K,V>& rhs)
+{
+ if (this == &rhs)
+  return *this;
+
+ // delete current
+ make_empty(root);
+ root = nullptr;
+ // build tree
+ std::vector<K> ks;
+ preorder(rhs.root, ks);
+
+ V val;
+ // find value that corresponds to current key, and insert into tree
+ for (int i = 0; i < ks.size(); ++i) {
+  find(ks[i], val);
+  insert(ks[i], val);
+ }
+
+ return *this;
+}
+
+template <typename K, typename V>
+void BSTCollection<K,V>::insert(const K& key, const V& val)
+{
+ // make a new leaf node
+ Node* curr = new Node;
+ curr->key = key;
+ curr->value = val;
+ curr->left = nullptr;
+ curr->right = nullptr;
+
+ // case: tree is empty
+ if (root == nullptr)
+  root = curr;
+
+ else {
+  Node* temp = root;
+  while (temp != nullptr) {
+   // case: the key that I'm inserting is less than the internal node "temp"
+   if (curr->key < temp->key) {
+    // leaf node found. Insert curr node into the tree as a new leaf.
+    if (temp->left == nullptr) {
+     temp->left = curr;
+     temp = nullptr;
+    }
+    // continue to traverse tree since "temp" is not a leaf
+    else 
+     temp = temp->left;
+   }
+   // case: the key that I'm inserting is greater than the internal node "temp"
+   else {
+    // leaf node found. Insert curr node into the tree as a new leaf node
+    if (temp->right == nullptr) {
+     temp->right = curr;
+     temp = nullptr;
+    }
+    // countinue to traverse tree since "temp" is not a leaf
+    else
+     temp = temp->right;
+   }
+  }
+ }
+ ++collection_size;
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::remove(const K& key)
+{
+ // ... Leave empty for now ...
+ // ... SAVE FOR HW 9 ...
+}
+
+
+template <typename K, typename V>
+bool BSTCollection<K,V>::find(const K& key, V& val) const
+{
+ Node* temp = root;
+ while (temp != nullptr) {
+  // key found in tree
+  if (temp->key == key) {
+   val = temp->value;
+   return true;
+  }
+  // temp node is greater than key. Traverse left
+  if (temp->key > key)
+   temp = temp->left;
+  // temp node is less than key. Traverse right
+  else
+   temp = temp->right;
+ }
+ return false;
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::
+range_search(const Node* subtree, const K& k1, const K& k2, std::vector<K>& ks) const
+{
+ // this is a recursive helper function
+ // base statement
+ if (subtree == nullptr)
+  return;
+
+ // modified inorder traversal. Only traverses nodes between k1 and k2 (inclusive)
+ // traverse left until k1 >= the subtree's key
+ if (k1 < subtree->key)
+  range_search(subtree->left, k1, k2, ks);
+ // insert qualifying nodes into vector
+ if (k1 <= subtree->key && k2 >= subtree->key)
+  ks.push_back(subtree->key);
+ // traverse right until k2 <= subtree's key
+ if (k2 > subtree->key)
+  range_search(subtree->right, k1, k2, ks);
+}
+
+
+template <typename K, typename V> 
+void BSTCollection<K,V>::find(const K& k1, const K& k2, std::vector<K>& ks) const
+{
+ // defer to the range search ( recursive ) helper function
+ range_search(root, k1, k2, ks);
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::inorder(const Node* subtree, std::vector<K>& ks) const
+{
+ // this is a recursive helper function
+ // base statement
+ if (subtree == nullptr)
+  return;
+ // left, "print", right
+ inorder(subtree->left, ks);
+ ks.push_back(subtree->key);
+ inorder(subtree->right, ks);
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::preorder(const Node* subtree, std::vector<K>& ks) const
+{
+ // this is a recursive helper function
+ // base statement
+ if (subtree == nullptr)
+  return;
+ // "print" --> left --> right
+ ks.push_back(subtree->key);
+ preorder(subtree->left, ks);
+ preorder(subtree->right, ks);
+}
+
+template <typename K, typename V>
+void BSTCollection<K,V>::keys(std::vector<K>& ks) const
+{
+ // defer to the inorder ( recursive ) helper function
+ inorder(root, ks);
+}
+
+
+template <typename K, typename V>
+void BSTCollection<K,V>::sort(std::vector<K>& ks) const
+{
+ // defer to the inorder ( recursive ) helper function
+ inorder(root, ks);
+}
+
+
+template <typename K, typename V>
+int BSTCollection<K,V>::size() const
+{
+ return collection_size;
+}
+
+
+template <typename K, typename V>
+int BSTCollection<K,V>::height(const Node* subtree_root) const
+{
+ // this is a recursive helper function
+ // base case
+ if (subtree_root == nullptr)
+  return 0;
+ //if (subtree_root->left == nullptr && subtree_root->right == nullptr)
+ // return 1;
+
+ return 1 + std::max(height(subtree_root->left), height(subtree_root->right));
+}
+
+
+template <typename K, typename V>
+int BSTCollection<K,V>::height() const
+{
+ // defer to the height ( recursive ) helper function
+ return height(root);
+}
+
+#endif
---
+++ HW8/hw8/hw8_tests.cpp
@@ -0,0 +1,287 @@
+/*Author: Eric Gustin
+  Assignment: CPSC223-01 HW08 hw8_tests.cpp
+  Description: This program tests the biunary search tree collection.
+  It tests every function in bst_collection.h, except for "remove"
+*/
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include "bst_collection.h"
+
+using namespace std;
+
+// Test 1
+TEST(BasicCollectionTest, CorrectSize) {
+ BSTCollection<string,double> c;
+ ASSERT_EQ(0, c.size());
+ c.insert("a", 10.0);
+ ASSERT_EQ(1, c.size());
+ c.insert("b", 20.0);
+ ASSERT_EQ(2, c.size());
+}
+// Test 2
+TEST(BasicCollectionTest, InsertAndFind) {
+ BSTCollection<string,double> c;
+ double v;
+ ASSERT_EQ(false, c.find("a", v));
+ c.insert("a", 10.0);
+ ASSERT_EQ(true, c.find("a", v));
+ ASSERT_EQ(v, 10.0);
+ ASSERT_EQ(false, c.find("b", v));
+ c.insert("b", 20.0);
+ ASSERT_EQ(true, c.find("b", v));
+ ASSERT_EQ(20.0, v);
+}
+/*
+// Test 3 -- THIS TEST SHOULD FAIL FOR HW8
+TEST(BasicCollectionTest, RemoveElems) {
+ BSTCollection<string,double> c;
+ c.insert("a", 10.0);
+ c.insert("b", 20.0);
+ c.insert("c", 30.0);
+ double v;
+ c.remove("a");
+ ASSERT_EQ(false, c.find("a", v));
+ ASSERT_EQ(true, c.find("b", v));
+ ASSERT_EQ(true, c.find("c", v));
+ c.remove("b");
+ ASSERT_EQ(false, c.find("b", v));
+ ASSERT_EQ(true, c.find("c", v));
+ c.remove("c");
+ ASSERT_EQ(false, c.find("c", v));
+ ASSERT_EQ(0, c.size());
+}
+*/
+// Test 4
+TEST(BasicCollectionTest, GetKeys) {
+ BSTCollection<string,double> c;
+ c.insert("a", 10.0);
+ c.insert("b", 20.0);
+ c.insert("c", 30.0);
+ vector<string> ks;
+ c.keys(ks);
+ vector< string>::iterator iter;
+ iter = find(ks.begin(), ks.end(), "a");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "b");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "c");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "d");
+ ASSERT_EQ(ks.end(), iter);
+}
+// Test 5
+TEST(BasicCollectionTest, GetKeysAdvanced) {
+ BSTCollection<string,double> c;
+ c.insert("g", 10.0);
+ c.insert("c", 20.0);
+ c.insert("b", 30.0);
+ c.insert("q", 10.0);
+ c.insert("f", 20.0);
+ c.insert("a", 30.0);
+ c.insert("d", 10.0);
+ c.insert("h", 20.0);
+ c.insert("t", 30.0);
+ c.insert("i", 10.0);
+ c.insert("qq", 20.0);
+ c.insert("qqq", 30.0);
+ vector<string> ks;
+ c.keys(ks);
+ for (int i = 0; i < ks.size()-1; ++i)
+  ASSERT_LE(ks[i], ks[i+1]);
+}
+// Test 6
+TEST(BasicCollectionTest, GetKeyRange) {
+ BSTCollection<string,double> c;
+ c.insert("a", 10.0);
+ c.insert("b", 20.0);
+ c.insert("c", 30.0);
+ c.insert("d", 40.0);
+ c.insert("e", 50.0);
+ vector<string> ks;
+ c.find("b", "d", ks);
+ vector<string>::iterator iter;
+ iter = find(ks.begin(), ks.end(), "b");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "c");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "d");
+ ASSERT_NE(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "a");
+ ASSERT_EQ(ks.end(), iter);
+ iter = find(ks.begin(), ks.end(), "e");
+ ASSERT_EQ(ks.end(), iter);
+}
+// Test 7
+TEST(BasicCollectionTest, GetKeyRangeAdvanced) {
+ BSTCollection<string,double> c;
+ c.insert("g", 10.0);
+ c.insert("c", 20.0);
+ c.insert("b", 30.0);
+ c.insert("q", 10.0);
+ c.insert("f", 20.0);
+ c.insert("a", 30.0);
+ c.insert("d", 10.0);
+ c.insert("h", 20.0);
+ c.insert("t", 30.0);
+ c.insert("i", 10.0);
+ c.insert("qq", 20.0);
+ c.insert("qqq", 30.0);
+ vector<string> ks;
+ c.find("cd", "q", ks);
+ ASSERT_EQ(ks.size(), 6);
+}
+// Test 8
+TEST(BasicCollectionTest, KeySort) {
+ BSTCollection<string,double > c;
+ c.insert("a", 10.0);
+ c.insert("e", 50.0);
+ c.insert("c", 30.0);
+ c.insert("b", 20.0);
+ c.insert("d", 40.0);
+ c.insert("aaa", 10.0);
+ c.insert("dfe", 50.0);
+ c.insert("chh", 30.0);
+ c.insert("zsdb", 20.0);
+ c.insert("zxsd", 40.0);
+ vector<string> sorted_ks;
+ c.sort(sorted_ks);
+ ASSERT_EQ(c.size(), sorted_ks.size());
+ for (int i = 0; i < int(sorted_ks.size())-1; ++i) {
+  ASSERT_LE(sorted_ks[i], sorted_ks[i+1]);
+ }
+}
+// Test 9
+TEST(BasicCollectionTest, AssignOpTest) {
+ BSTCollection<string,int> c1;
+ c1.insert("c", 10);
+ c1.insert("sdb", 15);
+ c1.insert("d", 20);
+ c1.insert("fga", 20);
+ c1.insert("kjhc", 10);
+ c1.insert("ddb", 15);
+ c1.insert("vcd", 20);
+ c1.insert("asa", 20);
+ BSTCollection<string,int> c2;
+ c2.insert("naan", 20);
+ c2.insert("none", 40);
+ c2 = c1;
+ vector<string> ks2;
+ c2.keys(ks2);
+ vector<string> ks1;
+ c1.keys(ks1);
+ for (int j = 0; j < ks2.size(); ++j)
+  ASSERT_EQ(ks1[j], ks2[j]);
+ ASSERT_EQ(c1.size(), c2.size());
+ ASSERT_EQ(c1.height(), c2.height());
+}
+// Test 10
+TEST(BasicCollectionTest, Height) {
+ BSTCollection<string,int> d;
+ d.insert("c", 10);
+ d.insert("b", 15);
+ d.insert("d", 20);
+ d.insert("a", 20);
+ d.insert("z", 10);
+ d.insert("f", 15);
+ d.insert("e", 20);
+ d.insert("g", 20);
+ ASSERT_EQ(d.height(), 5);
+}
+// Test 11
+TEST(BasicCollectionTest, CopyList) {
+ BSTCollection<string,int> w;
+ w.insert("hi", 11);
+ w.insert("computer", 12);
+ w.insert("forever", 122);
+ w.insert("never", 2110);
+ w.insert("bye", 11);
+ w.insert("laptop", 12);
+ w.insert("infinite", 122);
+ w.insert("none", 2110);
+ BSTCollection<string,int> w_copy = w;
+ ASSERT_EQ(w_copy.size(), w.size());
+
+ vector<string> w_keys;
+ vector<string> w_copy_keys;
+ w.sort(w_keys);
+ w_copy.sort(w_copy_keys);
+ for (int i = 0; i <= 3; ++i)
+ {
+  ASSERT_EQ(w_keys[i], w_copy_keys[i]);
+ }
+ // copy constructor with empty hash table
+ BSTCollection<string,int> empty;
+ BSTCollection<string,int> empty_copy(empty);
+ ASSERT_EQ(empty.size(), 0);
+ ASSERT_EQ(empty_copy.size(), 0);
+}
+// Test 12
+TEST(BasicCollectionTest, Assign) {
+ BSTCollection<string,int> y;
+ y.insert("gofish", 10);
+ y.insert("safety", 20);
+ y.insert("xyz", 30);
+ y.insert("ergo", 40);
+ y.insert("aapl", 50);
+ y.insert("good", 60);
+
+ BSTCollection<string,int> z;
+ z.insert("n", 10);
+ z.insert("a", 100);
+ z.insert("s", 1000);
+ z = y; 
+
+ ASSERT_EQ(z.size(), y.size());
+
+ vector<string> z_keys;
+ vector<string> y_keys;
+ y.keys(y_keys);
+ z.keys(z_keys);
+ ASSERT_EQ(z.size(), 6);
+ for (int i = 0; i <= 5; ++i)
+ {
+  ASSERT_EQ(y_keys[i], z_keys[i]);
+ }
+ // assignment operator will not do copying if trying to copy itself.
+ z = z;
+ ASSERT_EQ(z.size(), 6);
+ ASSERT_EQ(z.height(), y.height());
+}
+// Test 13
+TEST(BasicCollectionTest, Negatives) {
+ BSTCollection<double,string>* l = new BSTCollection<double,string>;
+ l->insert(999.0, "DigitalLogic");
+ l->insert(400.4, "AlgsAndDataStruct");
+ l->insert(-33.2, "discreteMath");
+ l->insert(-0.1, "Globals");
+ l->insert(0.0, "Human Nature");
+
+ vector<double> sorted_ints;
+ l->sort(sorted_ints);
+ for (int i = 0; i < int(sorted_ints.size()-1); ++i)
+ {
+  ASSERT_LE(sorted_ints[i], sorted_ints[i+1]);
+ }
+ delete l;
+}
+// Test 14
+TEST(BasicCollectionTest, SizeZero) {
+ BSTCollection<int,int>* m = new BSTCollection<int,int>;
+ ASSERT_EQ(m->size(), 0);
+ m->remove(2);
+ ASSERT_EQ(m->size(), 0);
+ int my_val;
+ ASSERT_EQ(m->find(0, my_val), false);
+ vector<int> keys_ints;
+ m->keys(keys_ints);
+ ASSERT_EQ(keys_ints.size(), 0);
+ m->sort(keys_ints);
+ ASSERT_EQ(keys_ints.size(), 0);
+ delete m;
+}
+int main(int argc, char** argv)
+{
+ testing::InitGoogleTest(&argc, argv);
+ return RUN_ALL_TESTS();
+}
